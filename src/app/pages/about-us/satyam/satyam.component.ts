9130149627e8fae373010d2f5d4afdf77910112e
vamseeuxd <vamsee.uxd@gmail.com>
2021-06-24 08:32:25 +0530

    Form Validation

--- src/app/pages/about-us/satyam/satyam.component.ts
+++ src/app/pages/about-us/satyam/satyam.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-satyam',
   templateUrl: './satyam.component.html',
-  styleUrls: ['./satyam.component.scss']
+  styleUrls: ['./satyam.component.scss'],
 })
 export class SatyamComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  subMitForm(form: NgForm) {
+    console.log(form.controls.firstName);
   }
-
 }
